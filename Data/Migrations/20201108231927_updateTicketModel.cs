490234794c0809e9986ca5e5380db8e5672850cc
John Clark <john.clark012@outlook.com>
2020-11-08 18:33:16 -0500

    Create ticket updates. Class presentation version

---
+++ Data/Migrations/20201108231927_updateTicketModel.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Semita3.Data.Migrations
+{
+    public partial class updateTicketModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Project_ProjectId",
+                table: "Ticket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ticket_ProjectId",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectTitle",
+                table: "Ticket");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketId",
+                table: "Project",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_TicketId",
+                table: "Project",
+                column: "TicketId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Ticket_TicketId",
+                table: "Project",
+                column: "TicketId",
+                principalTable: "Ticket",
+                principalColumn: "TicketId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Ticket_TicketId",
+                table: "Project");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_TicketId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "TicketId",
+                table: "Project");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectId",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectTitle",
+                table: "Ticket",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_ProjectId",
+                table: "Ticket",
+                column: "ProjectId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Project_ProjectId",
+                table: "Ticket",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,18 +8,43 @@ namespace Semita3.Models
 {
     public class Ticket
     {
-        public int ProjectId { get; set; }
+        public int ProjectId { get; }
         // navigation prop
-        public Project Project { get; set; }
-        public string ProjectTitle { get; set; }
+        //public Project Project { get; set; }
+        public string ProjectTitle { get; }
+
+        [Display(Name = "Ticket ID")]
+        [Required(ErrorMessage = "Ticket must have an ID.")]
         public int TicketId { get; set; }
+
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "Ticket must have a title.")]
         public string TicketTitle { get; set; }
+
+        [Display(Name = "Description")]
         public string TicketDescription { get; set; }
+
+        [Display(Name = "Submitter")]
+        [Required(ErrorMessage = "Must enter your name.")]
         public string TicketSubmitter { get; set; }
+
+        [Display(Name = "Priority")]
+        [Required(ErrorMessage = "Ticket priority number is required.")]
         public int TicketPriority { get; set; }
+
+        [Display(Name = "Status")]
+        [Required(ErrorMessage = "Open, in-progress, or complete.")]
         public string TicketStatus { get; set; }
+
+        [Display(Name = "Created On")]
+        [DataType(DataType.DateTime)]
         public DateTime TicketCreatedOn { get; set; }
+
+        [Display(Name = "Last Updated")]
+        [DataType(DataType.DateTime)]
         public DateTime TicketUpdatedOn { get; set; }
+
+        [Display(Name = "Comments")]
         public string TicketComment { get; set; }
     }
 }
